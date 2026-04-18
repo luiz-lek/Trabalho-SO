c39006c3113d83da2e3d965abae145695593be7d
luiz-lek <luiz_o@id.uff.br>
2026-04-18 13:30:53 -0300

    primeiro commit

--- Processo.py
+++ Processo.py
@@ -1,16 +1,5 @@
-from enum import Enum
-
-class Status(Enum):
-    NOVO = 1
-    PRONTO_SUSPENSO = 2
-    PRONTO = 3
-    EXECUTANDO = 4
-    SAIDA = 5
-    BLOQUEADO = 6
-    BLOQUEADO_SUSPENSO = 7
-
 class Processo:
-    def __init__(self, id, status, programa):
-        self.id = id
-        self.status = status
-        self.programa = programa
+    def __init__(self, codigo, dados, contexto):
+        self.codigo = codigo
+        self.dados = dados
+        self.contexto = contexto

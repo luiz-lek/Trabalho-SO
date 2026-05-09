704b2f3eea8c23c523e5a06ab3f01d131ae75715
Danilo Alexandre <daniloabs@id.uff.br>
2026-05-09 13:59:50 -0300

    Leitura de Processos da Entrada + Leitura de arquivo implementada + Criação de objetos Processo a partir da leitura dos dados do arquivo implementada + Leitura de lista de processos do arquivo implementada

--- Processo.py
+++ Processo.py
@@ -1,5 +1,10 @@
 class Processo:
-    def __init__(self, codigo, dados, contexto):
-        self.codigo = codigo
-        self.dados = dados
-        self.contexto = contexto
+    def __init__(self, idProcesso, durCpu1, durIO, durCpu2, tam):
+        self.idProcesso = idProcesso
+        self.durCpu1 = durCpu1
+        self.durIO = durIO
+        self.durCpu2 = durCpu2
+        self.tam = tam
+
+    def __str__(self):
+        return f"Processo {self.idProcesso}: CPU1={self.durCpu1}, IO={self.durIO}, CPU2={self.durCpu2}, TAM={self.tam}"

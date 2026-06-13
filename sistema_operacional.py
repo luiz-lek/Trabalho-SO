70745e73aca299c0b61be2de77b3e2af351fda90
luiz-lek <luiz_o@id.uff.br>
2026-06-13 16:13:16 -0300

    Loop principal de execução
    +loop de execução na classe de sistema operacional
    
    +Verificações de recursos disponíveis para a admissão de um processo novo.

--- sistema_operacional.py
+++ sistema_operacional.py
@@ -12,28 +12,28 @@ class SistemaOperaciona:
     def __init__(self):
         # Todos os componentes do sistema operacional.
         self.memoria_principal = MemoriaPrincipal()
-        self.escalonador = Escalonador(self.memoria_principal)
+        self.escalonador = Escalonador()
         self.despachante = Despachante()
         self.cpus: list[CPU] = [CPU(i) for i in range(4)]
         self.dma = DMA()
 
         processos = alistaProcessos("entrada.txt", self.despachante)
         for processo in processos:
-            self.escalonador.escalonar_processo_novo(processo)
+            self.escalonador.enfileirar_processo_novo(processo)
 
     def executar(self) -> None:
-        for i in range(20):
+        for i in range(21):
             print(f"\n--- Tique {i} ---")
             self.clock_cpus()
             processos_desbloqueados = self.dma.clock()
             for processo_concluido in processos_desbloqueados:
-                self.escalonador.escalonar_processo_bloqueado(processo_concluido.id)
+                self.escalonador.desbloquer_processo(processo_concluido)
 
     def clock_cpus(self) -> None:
         '''
-        Como o programa é sequencial, não podemos por um processo que acabou de sair da cpu direto no escalonador
-        pois corremos o risco de um processo sair da cpu 0, por exemplo e ganhar cpu de novo se a cpu 1 tiver 
-        livre, ganhar cpu de novo no mesmo clock.
+            Como o programa é sequencial, não podemos por um processo que acabou de sair da cpu direto no escalonador
+            pois corremos o risco de um processo sair da cpu 0, por exemplo e ganhar cpu de novo se a cpu 1 tiver 
+            livre, ganhar cpu de novo no mesmo clock.
         '''
         processos_perderam_cpu: list[Processo] = list()
 
@@ -41,16 +41,39 @@ class SistemaOperaciona:
         for cpu in self.cpus:
             # Escalona um novo processo se ela estiver livre
             if cpu.estado == Estado.Vazio:
-                processo = self.escalonador.escalonar_processo_para_execucao()
+                processo = self.escalonador.selecionar_proximo_processo()
                 self.despachante.despachar(processo, cpu)
 
-            # Escalona se o processo executado na cpu foi desalocado.
+            # Insere na lista se o processo executado na cpu foi desalocado.
             processo_interrompido: Processo | None = cpu.clock()
             if processo_interrompido is not None:
                 processos_perderam_cpu.append(processo_interrompido)
             print()
 
+        # O escalonador insere na devida fila todos os processos que perderam cpu
         for processo in processos_perderam_cpu:
-            self.escalonador.escalonar_processo_interrompido(processo)
+            self.escalonador.tratar_retorno_cpu(processo)
 
-        time.sleep(0.5)
+            if processo.estado == EstadoProcesso.BLOQUEADO:
+                self.dma.adicionar_processo(processo)
+
+            elif processo.estado == EstadoProcesso.FINALIZADO:
+                self.memoria_principal.desalocar_processo(processo)
+
+        self.tentar_admitir_processo_novo()
+
+        time.sleep(1)
+
+    def tentar_admitir_processo_novo(self) -> None:
+        while 1 < 2:
+            processo = self.escalonador.retirar_proximo_novo()
+            if processo is None:
+                break
+
+            tem_memoria: int | None = self.memoria_principal.verificar_bloco_disponivel(processo.tam)
+            if tem_memoria is None:
+                self.escalonador.enfileirar_processo_novo(processo)
+                return
+            
+            self.memoria_principal.alocar_processo(tem_memoria, processo)
+            self.escalonador.admitir_processo(processo)

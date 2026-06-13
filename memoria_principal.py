09d5760f570a0b0025040c3740d990e3eedc7dae
luiz-lek <luiz_o@id.uff.br>
2026-06-13 14:18:51 -0300

    Mudança na lógica da memória principal.
    A memória antiga alocava o processo a partir da primeira posição livre encontrada e ignorava a possibilidade de ter um processo alocado dentro do intervalo.  Agora ela verifica se no espaço livre encontrado não tem nenhum outro processo.

--- memoria_principal.py
+++ memoria_principal.py
@@ -3,29 +3,46 @@ from processos import *
 class MemoriaPrincipal:
     def __init__(self):
         # Lista de alocação (0 para vazio e 1 para alocado)
-        self.alocacao = [-1] * 32000
+        self.tam_memoria = 32000
+        self.alocacao = [None] * self.tam_memoria
 
-        # Lista de processos
-        self.processos = []
+    
+    def alocar_processo(self, inicio: int, processo: Processo):
+        if inicio + processo.tam > len(self.alocacao):
+            raise IndexError("A alocação estora o limite do vetor")
+        
+        for i in range(inicio, inicio + processo.tam):
+            self.alocacao[i] = processo.id
 
-    def aloca(self, p: Processo) -> None:
-        # Verificando se há espaço suficiente para alocar o processo
-        for i in range(len(self.alocacao)):
-            if self.alocacao[i] == -1: # Encontrou um espaço vazio
-                if i + p.tam <= len(self.alocacao): # Verifica se o processo cabe no espaço vazio restante
+    def desalocar_processo(self, processo: Processo):
+        for i in range(self.tam_memoria):
+            if self.alocacao[i] == processo.id:
+                j = i
+                while j < self.tam_memoria and self.alocacao[j] == processo.id:
+                    self.alocacao[j] = None
+                    j+=1
+                break
+
+    def verificar_bloco_disponivel(self, tam_bloco: int) -> int:
+        i = 0
+        tam_mem = len(self.alocacao)
+        
+        while i <= tam_mem - tam_bloco:
+            bloco_livre = True
+            
+            for j in range(tam_bloco):
+                if self.alocacao[i + j] != None:
+                    bloco_livre = False
                     
-                    # Faz a alocação do processo na memória
-                    for j in range(i, i + p.tam):
-                        self.alocacao[j] = p.id
-
-                    self.processos.append(p) # Adiciona o processo à lista de processos alocados
-                    print(f"Processo {p.id} alocado na memória.")
-                    p.pos_memoria = i
-                    return
+                    i = i + j + 1
+                    break
+            
+            if bloco_livre:
+                return i
                 
-        print(f"Não há espaço suficiente para alocar o processo {p.id}.")
+        return None
 
-    def desaloca(self, p: Processo):
+    def desalocar(self, p: Processo):
         if p not in self.processos:
             print(f"Processo {p.id} não encontrado na memória.")
             return
